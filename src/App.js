c811d5b246d822ece99d30c690c3e53767377873
Luis <luisphelipecode@gmail.com>
2020-06-16 14:47:41 -0300

    socket.io-client for python socketio server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:8000";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.on("connect", (data) => {
+      console.log("data received from connect:", data);
+      setResponse((data && data["message"]) || "empty...");
+    });
+
+    socket.on((data) => {
+      console.log("data received from send:", data);
+      setResponse((data && data["message"]) || "empty...");
+    });
+
+    socket.on("job_socket_test", (data) => {
+      console.log("data received from job_socket_test:", data);
+      setResponse((data && data["message"]) || "empty...");
+    });
+
+    socket.on("message", (data) => {
+      console.log("data received from message:", data);
+      setResponse((data && data["message"]) || "empty...");
+    });
+  }, []);
+
+  return <div>Response: {response}</div>;
 }
 
 export default App;
