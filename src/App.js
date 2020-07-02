6db1144c7c0d32ce7de1c8a907e226932d3d42db
Luis <luisphelipecode@gmail.com>
2020-07-02 07:56:21 -0300

    add connect_data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://pdv-exchange-dev.herokuapp.com";
+import axios from "axios";
+
+const HEROKU_ENDPOINT = "https://pdv-exchange-dev.herokuapp.com";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTkzMTgwNjYyfQ.MHW_QsE9RNVqdxbGcMBjiynV7J7orUqBAl-Sy8-W2p0";
 
 function App() {
   const [response, setResponse] = useState("");
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
+    const socket = socketIOClient(HEROKU_ENDPOINT);
+    // const socket = socketIOClient(":3000");
 
     socket.on("connect", (data) => {
       console.log("data received from connect:", data);
+      setResponse((data && data["message"]) || "empty...");
+    });
+
+    socket.on("connect_data", (data) => {
+      console.log("data received from connect_data:", data);
       setResponse((data && data["message"]) || "empty...");
     });
 
@@ -18,6 +28,27 @@ function App() {
       setResponse((data && data["message"]) || "empty...");
     });
   }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     console.log("fetching");
+  //     const response = await axios.get(
+  //       "https://pdv-exchange-dev.herokuapp.com/ohlc",
+  //       {
+  //         interval: 60,
+  //         since: 1593400319,
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${TOKEN}`,
+  //         },
+  //       }
+  //     );
+  //     console.log(response);
+  //     console.log(response.data);
+  //     console.log(response.data.length);
+  //   })();
+  // }, []);
 
   return <div>Response: {response}</div>;
 }
